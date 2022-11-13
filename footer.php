8c4bd4968d2da316a6e27d9350ecad21bb8d07d1
Reija Mata <113746733+reijamata@users.noreply.github.com>
2022-11-13 16:55:23 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,109 @@
+<footer class="bg-dark text-light">
+<div class="container">
+<div class="f-items default-padding">
+<div class="row">
+<div class="col-lg-5 col-md-6 item">
+<div class="f-item about">
+<h4 class="widget-title">About aveit</h4>
+<p>
+Required honoured trifling eat pleasure man relation. Assurance yet bed was improving furniture man. Distrusts delighted Excuse few the remain highly feebly add people manner say. It high at my mind by roof.
+</p>
+<div class="social">
+<ul>
+<li>
+<a href="#">
+<i class="fab fa-facebook-f"></i>
+</a>
+</li>
+<li>
+<a href="#">
+<i class="fab fa-twitter"></i>
+</a>
+</li>
+<li>
+<a href="#">
+<i class="fab fa-linkedin-in"></i>
+</a>
+</li>
+<li>
+<a href="#">
+<i class="fab fa-behance"></i>
+</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-lg-2 col-md-6 item">
+<div class="f-item link">
+<h4 class="widget-title">Services</h4>
+<div class="menu-footer-link-1-container"><ul id="menu-footer-link-1" class="menu"><li id="menu-item-1138" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1138"><a>SEO Marketing</a></li>
+<li id="menu-item-1139" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1139"><a href="">Pay Per Click</a></li>
+<li id="menu-item-1140" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1140"><a href="">SEO Services</a></li>
+<li id="menu-item-1141" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1141"><a href="">Social Media</a></li>
+<li id="menu-item-1142" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1142"><a href="">SEO Audit</a></li>
+</ul></div> </div>
+</div>
+ <div class="col-lg-2 col-md-6 item">
+<div class="f-item link">
+<h4 class="widget-title">Company</h4>
+<div class="menu-footer-link-2-container"><ul id="menu-footer-link-2" class="menu"><li id="menu-item-1144" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1144"><a href="">About Us</a></li>
+<li id="menu-item-1143" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1143"><a href="">Contact Us</a></li>
+<li id="menu-item-1147" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1147"><a href="/">Career</a></li>
+<li id="menu-item-1146" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1146"><a href="">Terms</a></li>
+<li id="menu-item-1145" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1145"><a href="">Team Members</a></li>
+</ul></div> </div>
+</div>
+<div class="col-lg-3 col-md-6 item">
+<div class="f-item contact">
+<h4 class="widget-title">Contact Us</h4>
+<div class="address">
+<ul>
+<li>
+<p>ADDRESS</p>
+<strong>California, TX 70240</strong>
+</li>
+<li>
+<p>EMAIL</p>
+<strong><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="186b6d6868776a6c586e7974717c6c707d757d367b7775">[email&#160;protected]</a></strong>
+</li>
+<li>
+<p>CONTACT</p>
+<strong>+44-20-7328-4499</strong>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="footer-bottom">
+<div class="row align-center">
+<div class="col-lg-4">
+<p>&copy; Copyright 2021 parasabahy</p>
+</div>
+<div class="col-lg-4 text-center logo">
+<a href="#"><img src="https://wordpressriverthemes.com/aveitelementor/wp-content/themes/aveit/img/logo-light.png" alt="Logo"></a>
+</div>
+<div class="col-lg-4 text-right newsletter">
+<form action="#">
+<div class="input-group stylish-input-group">
+<input type="email" placeholder="Enter your e-mail" class="form-control" name="email">
+<span class="input-group-addon">
+<button type="submit">
+    <a href="#"></a>
+<i class="fa fa-paper-plane"></i>
+</button>
+</span>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+    
+    <script id='elementor-frontend-js-before'>
+var elementorFrontendConfig = {"environmentMode":{"edit":false,"wpPreview":false,"isScriptDebug":false},"i18n":{"shareOnFacebook":"Share on Facebook","shareOnTwitter":"Share on Twitter","pinIt":"Pin it","download":"Download","downloadImage":"Download image","fullscreen":"Fullscreen","zoom":"Zoom","share":"Share","playVideo":"Play Video","previous":"Previous","next":"Next","close":"Close"},"is_rtl":false,"breakpoints":{"xs":0,"sm":480,"md":768,"lg":1025,"xl":1440,"xxl":1600},"responsive":{"breakpoints":{"mobile":{"label":"Mobile","value":767,"default_value":767,"direction":"max","is_enabled":true},"mobile_extra":{"label":"Mobile Extra","value":880,"default_value":880,"direction":"max","is_enabled":false},"tablet":{"label":"Tablet","value":1024,"default_value":1024,"direction":"max","is_enabled":true},"tablet_extra":{"label":"Tablet Extra","value":1200,"default_value":1200,"direction":"max","is_enabled":false},"laptop":{"label":"Laptop","value":1366,"default_value":1366,"direction":"max","is_enabled":false},"widescreen":{"label":"Widescreen","value":2400,"default_value":2400,"direction":"min","is_enabled":false}}},"version":"3.6.4","is_static":false,"experimentalFeatures":{"e_dom_optimization":true,"e_optimized_assets_loading":true,"e_optimized_css_loading":true,"a11y_improvements":true,"e_import_export":true,"additional_custom_breakpoints":true,"e_hidden_wordpress_widgets":true,"landing-pages":true,"elements-color-picker":true,"favorite-widgets":true,"admin-top-bar":true},"urls":{"assets":"https:\/\/wordpressriverthemes.com\/aveitelementor\/wp-content\/plugins\/elementor\/assets\/"},"settings":{"page":[],"editorPreferences":[]},"kit":{"active_breakpoints":["viewport_mobile","viewport_tablet"],"global_image_lightbox":"yes","lightbox_enable_counter":"yes","lightbox_enable_fullscreen":"yes","lightbox_enable_zoom":"yes","lightbox_enable_share":"yes","lightbox_title_src":"title","lightbox_description_src":"description"},"post":{"id":585,"title":"OP%20Software%20Landing%20H5%20%E2%80%93%20Aveit%20Elementor","excerpt":"","featuredImage":false}};
+</script>
+<script src='https://wordpressriverthemes.com/aveitelementor/wp-content/plugins/elementor/assets/js/frontend.min.js?ver=3.6.4' id='elementor-frontend-js'></script>
+</footer>
---
+++ sidebar.php
@@ -0,0 +1,49 @@
+<header id="home">
+
+<nav class="navbar navbar-default navbar-fixed dark no-background bootsnav">
+
+<div class="top-search">
+<div class="container">
+<div class="input-group">
+<span class="input-group-addon"><i class="fa fa-search"></i></span>
+<input type="text" class="form-control" placeholder="Search">
+<span class="input-group-addon close-search"><i class="fa fa-times"></i></span>
+</div>
+</div>
+</div>
+
+<div class="container-full">
+
+<div class="attr-nav">
+<ul class="button">
+<li>
+<a href="#">LOGIN</a>
+</li>
+<li>
+<a href="#">SIGN UP</a>
+</li>
+</ul>
+</div>
+
+
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+<i class="fa fa-bars"></i>
+</button>
+<a class="navbar-brand" href="">
+<img src="https://wordpressriverthemes.com/aveitelementor/wp-content/themes/aveit/img/logo-blue.png" class="logo" alt="Logo">
+</a>
+</div>
+
+
+<div id="navbar-menu" class="collapse navbar-collapse"><ul data-in="#" data-out="#" class="nav navbar-nav navbar-center" id="menu-h5-op-software-landing"><li id="menu-item-1210" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1210"><a title="Home" href="#home">Home</a></li>
+<li id="menu-item-1211" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1211"><a title="Features" href="#features">Features</a></li>
+<li id="menu-item-1212" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1212"><a title="Overview" href="#overview">Overview</a></li>
+<li id="menu-item-1213" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1213"><a title="FAQ" href="#faq">FAQ</a></li>
+<li id="menu-item-1214" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1214"><a title="Pricing" href="#pricing">Pricing</a></li>
+<li id="menu-item-1215" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1215"><a title="Blog" href="#blog">Blog</a></li>
+</ul></div> 
+</div>
+</nav>
+    
+</header>
